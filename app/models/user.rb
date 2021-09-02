c90a6bd07a9c1cdfc6e936f111adc6c47b937877
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-09-02 17:57:44 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
+
+  has_many :items
+  
 end
