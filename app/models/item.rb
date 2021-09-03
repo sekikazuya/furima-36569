216af8f4ab1b31961902559e6136c45031126e40
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-09-03 16:26:34 +0900

    各モデルの作成、アソシエーション・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
   belongs_to :prefecture
+  belongs_to :delivery_time
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :introduction, :price, presence: true
+  validates :category_id, :status_id, :shipping_cost_id, :prefecture_id, :delivery_time_id, numericality: {other_than: 0, message: "Can't be blank"}
 end
