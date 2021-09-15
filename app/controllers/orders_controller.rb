b599afc389b6759e3b616d1f77351701c40eb52e
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-09-15 15:42:16 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -23,7 +24,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code,
+                :prefecture_id,
+                :city,
+                :house_number,
+                :building_name,
+                :phone_number,
+                :user_id,
+                :item_id,
+                :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number
+    validates :user_id,
+              :item_id,
+              :city,
+              :house_number,
+              :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
   end
